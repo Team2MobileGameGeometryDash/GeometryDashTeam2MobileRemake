781235c1cd59dcddeacdeb7b25f02d9f99021a1a
Lollo48 <loory4895@gmail.com>
2023-10-13 10:46:18 +0200

    added slider
    added slider for map tracking

--- Assets/Scripts/Player/Manager/PlayerUIManager.cs
+++ Assets/Scripts/Player/Manager/PlayerUIManager.cs
@@ -5,5 +5,39 @@ using TMPro;
 public class PlayerUIManager : MonoBehaviour
 {
     
+    public Transform EndMap;
+    Slider _slider;
+
+    private void Awake()
+    {
+        _slider = GetComponentInChildren<Slider>();
+        DistanceToEnd();
+    }
+
+
+    private void Update()
+    {
+        UpdateSlider();
+    }
+
+    private void DistanceToEnd()
+    {
+
+        float distance = Vector2.Distance(transform.position, EndMap.position);
+        _slider.maxValue = distance;
+
+    }
+
+    private void UpdateSlider()
+    {
+        if(_slider.value == _slider.maxValue)
+        {
+            _slider.value = _slider.maxValue;
+            return;
+        }
+        _slider.value = transform.position.x;
+        
+    }
+
 
 }
