1f95d7372403ccefce19f518771a857c7cf85078
loampo <loampo1@gmail.com>
2023-10-18 21:37:33 +0200

    menu WIP

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class MainMenu : MonoBehaviour
+{
+    #region VariablesInInspector
+
+    public Canvas MainMenuScreen;
+    public Canvas CreditScreen;
+    public Canvas OptionsScreen;
+
+    #endregion
+
+
+    #region ButtonsFunctions
+
+    /// <summary>
+    ///  for PLAY button
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    /// <summary>
+    /// for QUIT button
+    /// </summary>
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Credits panel
+    /// </summary>
+    public void ToggleCredits()
+    {
+        ToggleUIScreen(CreditScreen, MainMenuScreen);
+    }
+
+    /// <summary>
+    /// Options screen handler
+    /// </summary>
+    public void ToggleOptions()
+    {
+        ToggleUIScreen(OptionsScreen, MainMenuScreen);
+    }
+
+    /// <summary>
+    /// Used to switch panel
+    /// </summary>
+    /// <param name="selectedScreen">the panel you want to active or disactive</param>
+    /// <param name="previousScreen">the previous panel</param>
+    public void ToggleUIScreen(Canvas selectedScreen, Canvas previousScreen)
+    {
+        if (!selectedScreen.gameObject.activeInHierarchy)
+        {
+            selectedScreen.gameObject.SetActive(true);
+            previousScreen.gameObject.SetActive(false);
+            return;
+        }
+        else
+        {
+            previousScreen.gameObject.SetActive(true);
+            selectedScreen.gameObject.SetActive(false);
+            return;
+        }
+    }
+
+    #endregion
+}
