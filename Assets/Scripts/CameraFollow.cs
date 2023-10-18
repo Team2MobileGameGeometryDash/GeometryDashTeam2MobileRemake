60ebd37f1ecdf2439aabe1b489909e0e77ff8230
Lollo48 <loory4895@gmail.com>
2023-10-18 12:29:04 +0200

    fix camera
    fix camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform Target; 
-    public float smoothSpeed = 0.125f;
+    public float smoothSpeed;
     public Vector3 Offset;
 
 
@@ -18,7 +18,7 @@ public class CameraFollow : MonoBehaviour
     private void HandleCameraFollow()
     {
         Vector3 desiredPosition =  new Vector3(Target.position.x + Offset.x,Offset.y,0f);
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * 100f * Time.deltaTime);
         smoothedPosition.z = transform.position.z;
         transform.position = smoothedPosition;
     }
