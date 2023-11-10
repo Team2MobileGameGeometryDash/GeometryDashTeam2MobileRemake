36005b7db68cf5fd7a6b4da6f70dd9523eb5d701
loampo <loampo1@gmail.com>
2023-11-10 02:04:38 +0100

    Fix Coin Anim
    New Animation and add animator on Coins Script

--- Assets/Scripts/Coins.cs
+++ Assets/Scripts/Coins.cs
@@ -8,10 +8,26 @@ public class Coins : MonoBehaviour
     [Range(1,3)]
     public int CoinNumber;
     private bool isCollected;
+    private bool isAlreadyCollected
+    {
+        get
+        {
+            if (PlayerPrefs.GetInt(SceneManager.GetActiveScene().name + (CoinNumber - 1)) == 1) return true;
+            else return false;
+        }
+    }
 
+    Animator animator;
+
+    private void OnEnable()
+    {
+        animator.SetBool("isAlreadyCollected", isAlreadyCollected);
+    }
     private void Start()
     {
         isCollected = false;
+        animator = GetComponent<Animator>();
+        animator.SetBool("isAlreadyCollected", isAlreadyCollected);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -20,14 +36,19 @@ public class Coins : MonoBehaviour
         if (collision.TryGetComponent(out PlayerController player))
         {
             isCollected = true;
-            gameObject.SetActive(false);
-            
+            animator.SetTrigger("Collected");            
         }
+    }
+
+    public void CoinDisable()
+    {
+        gameObject.SetActive(false);
     }
 
     public void ResetCoins()
     {
         isCollected = false;
+        if (gameObject.activeInHierarchy) gameObject.SetActive(false); //If anyone dead in animation progress
         gameObject.SetActive(true);
     }
 
